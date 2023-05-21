e28e45f37f54afa9ae2a7c9afccefa7734303596
winklermi <winkler.miriam@gmx.net>
2023-05-21 10:35:46 +0200

    ExplainaBoard evaluation files and small adjustments in intent-reformat-input.py

--- evaluation/ExplainaBoard/intent-reformat-input.py
+++ evaluation/ExplainaBoard/intent-reformat-input.py
@@ -3,21 +3,24 @@ import sys
 file = sys.argv[1]
 outfile = sys.argv[2]
 
-sent = []
+sent = {}
 with open(file, "r", encoding="utf-8") as infile, open(outfile, "a", encoding="utf-8") as outfile:
     for line in infile.readlines()[:-1]:
         line = line.strip()
 
         if line == "":
-            outfile.write(f"{sent[1]}\t{sent[0]}\n")
-            sent = []
+            outfile.write(f"{sent.get('sentence')}\t{sent.get('intent')}\n")
+            sent = {}
 
         if line.startswith("#"):
             _, cat, *content = line.split()
             
-            if cat == "intent:" or cat == "text:":
-                sent.append(" ".join(content))
+            if cat == "intent:":
+                sent['intent'] = " ".join(content)
+
+            if cat == "text:":
+                sent['sentence'] = " ".join(content)
     
-    outfile.write(f"{sent[1]}\t{sent[0]}\n")
+    outfile.write(f"{sent.get('sentence')}\t{sent.get('intent')}\n")
 
     
